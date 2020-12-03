83b7066ab9186d15d3c07e3728201088689a0738
Ryan Tan <ryan111tan@berkeley.edu>
2020-12-02 16:39:00 -0800

    Initial Commit

---
+++ Project productivity/Project_productivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_productivityApp.swift
+//  Project productivity
+//
+//  Created by Ryan Tan on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_productivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
